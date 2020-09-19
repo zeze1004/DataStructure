fb80ec116a749a80052249e7a4edc833c9bfacbe
zeze1004 <thwjd2717@naver.com>
2020-09-20 06:22:30 +0900

    재귀함수+스택 이제 이해함..^^!

--- Hanoi.cpp
+++ Hanoi.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
-
-void hanoi(int n, char from, char tmp, char to) {
-    if(n==1){
+int count1  = 0;
+int count2  = 0;
+int count3  = 0;
+void hanoi(int n, char from, char tmp, char to) {       
+    if(n==1){   
+        // 어느 원반이 어느 기둥에서 어느 기둥으로 옮기는지 출력        
         printf("%d를 %c에서 %c로 옮기다 \n", n,from,to);
-    } else {
-        hanoi(n-1,from,to,tmp);
-        printf("%d를 %c에서 %c로 옮기다 \n", n,from,tmp);
-        hanoi(n-1,tmp,from,to);
+    } else {    
+        // 마지막 원반을 제외한 n-1개 원반을 A에서 C를 이용해 B로 이동
+        hanoi(n-1,from,to,tmp);                                             
+        printf("%d를 %c에서 %c로 옮기다 \n", n,from,tmp);   
+        //  n-1 원반을 B에서 A를 이용해 C로 이동
+        hanoi(n-1,tmp,from,to);                         
     }
 }
 
