1c17a2a3a0150f0d9d2e499132984854a07f872a
zeze1004 <thwjd2717@naver.com>
2020-09-20 22:46:28 +0900

    자료구조 과제 도움을 주신 분들
    Co-authored-by: adgjl16430@gmail.com <adgjl16430@gmail.com>
    Co-authored-by: GF <gunhoflash@gmail.com>

--- Hanoi.cpp
+++ Hanoi.cpp
@@ -1,15 +1,15 @@
-#include <stdio.h>
-int count1  = 0;
-int count2  = 0;
-int count3  = 0;
+#include <iostream>
+using namespace std;
+
 void hanoi(int n, char from, char tmp, char to) {       
     if(n==1){   
         // 어느 원반이 어느 기둥에서 어느 기둥으로 옮기는지 출력        
-        printf("%d를 %c에서 %c로 옮기다 \n", n,from,to);
+        cout << n << " :" <<from <<"->" << to <<'\n';
     } else {    
         // 마지막 원반을 제외한 n-1개 원반을 A에서 C를 이용해 B로 이동
         hanoi(n-1,from,to,tmp);                                             
-        printf("%d를 %c에서 %c로 옮기다 \n", n,from,tmp);   
+        cout << n << " :" <<from <<"->" << to <<'\n';
+          
         //  n-1 원반을 B에서 A를 이용해 C로 이동
         hanoi(n-1,tmp,from,to);                         
     }
--- hanoi_iteration.cpp
+++ hanoi_iteration.cpp
@@ -1,49 +1,57 @@
 #include <iostream>
 #include <stack>
+#include <vector>
+
 using namespace std;
+  
+// n개의 원판을 from에서 to로 이동시키자 (temp 기둥을 이용할 수 있음)
+void hanoi(int n, char from, char temp, char to) {
 
-void hanoi(int n, char from, char tmp, char to) {
-    // int형 stack 호출
-    stack<int> s; 
-    while (1)
-    {
-        while(n>1) {     // N= 3,2 , 반복문 두 번 돈다 
-			/*
-			C
-			B
-			A
-			4
-			*/
-            s.push(n), 
-            s.push(from),
-			s.push(tmp),
-            s.push(to);
-			// 2
-            n = n - 1;                   
-        }
-		// 원반 
-        cout << "원반 " << n << "을 " << from << "에서 " << tmp <<" 로 이동시킨다." << '\n';
-        
-		if (s.size()) {
-            to = s.top(),
-			     s.pop();
+   // stack을 이용하자
+   stack<vector<int>> s; // [ n, type, from, temp, to ]
+   vector<int> poped;
 
-			from = s.top(),
-			       s.pop();
-			
-			n = s.top(),
-			    s.pop();
-            
-            cout << "원반 " << n << "을 " << from << "에서 " << to <<" 로 이동시킨다." << '\n'; 
-            n = n-1;
-		}
-		else {
-			break;
-		}
-    }
+   // 4번째 원판을 A에 B를 거쳐 C로 : 4 0 A B C => 0 이면 n번째 원판
+   // 4개 원판들을 A에 B를 거쳐 C로 : 4 1 A B C => 1 이면 n개의 원판들
+
+
+   // 맨 처음에, 스택에 이 명령을 넣는다.
+   // 명령: n from temp to
+   s.push({ n, 1, (int)from, (int)temp, (int)to });                       //배열로 명령어를 넣어준다.
+
+   // 반복: stack이 비어있지 않는 한
+   while (s.size() != 0) {
+
+      // 스택 맨 위를 읽는다(가져온다).
+      poped = s.top(); // 4 0 A B C
+      s.pop();
+
+      // 1. 만약: 1개의 원판을 이동하는 명령이라면
+      if (poped[1] == 0) {
+         // 이동내용을 출력한다.
+         cout << poped[0] << ": " << (char)poped[2] << "->" << (char)poped[4] << '\n';
+      } else {
+         // 1층짜리 원판이면, 이동내용을 출력한다.
+         if (poped[0] == 1) {
+            cout << poped[0] << ": " << (char)poped[2] << "->" << (char)poped[4] << '\n';
+         } else {
+            // 2. 그게 아니라면:
+
+            //// 명령(n a b c : n개를 a에서 b를 거쳐 c에 이동)을 세 단계로 나누고 스텍은 반대로 넣어야한다.
+            // 스택에 [n-1개 b a c] 라는 명령을 넣는다.
+            s.push({poped[0] - 1, 1, poped[3], poped[2], poped[4]});
+
+            // 스택에 [n번째 a b c] 라는 명령을 넣는다.
+            s.push({poped[0], 0, poped[2], poped[3], poped[4]});
+
+            // 스택에 [n-1개 a c b] 라는 명령을 넣는다.
+            s.push({poped[0] - 1, 1, poped[2], poped[4], poped[3]});
+         }
+      }
+   }
 }
 
 int main() {
    hanoi(4, 'A','B','C');
-    return 0;
+   return 0;
 }
