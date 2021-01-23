ebb671bb96b4e20b0adfea79bc14e773e449b418
zeze1004 <thwjd2717@naver.com>
2021-01-24 02:02:29 +0900

    플로이드 구현

---
+++ Chap11_Graph/Floyd.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+#define max_vertices 100
+#define INF 1000000
+
+typedef struct GraphType
+{
+	int n; // 정점의 개수
+	int weight[max_vertices][max_vertices];
+} GraphType;
+
+int graph[max_vertices][max_vertices];
+
+// 출력 함수
+void print_graph(GraphType *g)
+{
+	int i, j;
+	cout << "------------------------\n";
+	for (i = 0; i < g->n; i++)
+	{
+		for (j = 0; j < g->n; j++)
+		{
+			if (graph[i][j] == INF)
+				cout << " * ";
+		}
+		cout << '\n';
+	}
+	cout << "------------------------\n";
+}
+
+void floyd(GraphType *g)
+{
+	int i, j, k;
+	// 자신 노드와 연결된 노드의 가중치만 저장
+	for (i = 0; i < g->n; i++)
+	{
+		for (j = 0; j < g->n; j++)
+		{
+			graph[i][j] = g->weight[i][j];
+			cout << "g->weight[" << i << "][" << j << "]" << '\n';
+		}
+	}
+	print_graph(g);
+	cout << "first print_graph(" << g << ")" << '\n';
+
+	// k 노드와 거쳐서 목표 노드를 갈 때 원래 가중치 값 보다 적으면 갱신
+	for (k = 0; k < g->n; k++)
+	{
+		for (i = 0; i < g->n; i++)
+		{
+			for (j = 0; j < g->n; j++)
+			{
+				if (graph[i][k] + graph[k][j] < graph[i][j])
+					graph[i][j] = graph[i][k] + graph[k][j];
+			}
+		}
+		print_graph(g);
+		cout << "second print_graph(" << g << ")" << '\n';
+	}
+}
+
+int main()
+{
+	ios_base ::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	GraphType g = {7,
+								 {{0, 7, INF, INF, 3, 10, INF},
+									{7, 0, 4, 10, 2, 6, INF},
+									{INF, 4, 0, 2, INF, INF, INF},
+									{INF, 10, 2, 0, 11, 9, 4},
+									{3, 2, INF, 11, 0, INF, 5},
+									{10, 6, INF, 9, INF, 0, INF},
+									{INF, INF, INF, 4, 5, INF, 0}}};
+	floyd(&g);
+	return 0;
+}
